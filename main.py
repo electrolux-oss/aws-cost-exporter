d8dda1be4e31aed002fe09ee6f104b9197bf423f
Long Zhang <long.zhang@electrolux.com>
2024-03-01 15:38:59 +0100

    * #6 add more validations for the config file * #6 fix typos

--- main.py
+++ main.py
@@ -1,43 +1,57 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-# Filename: exporter.py
+# Filename: main.py
 
+import argparse
+import logging
 import os
 import sys
-import argparse
-from app.exporter import MetricExporter
+
 from envyaml import EnvYAML
 from prometheus_client import start_http_server
-import logging
+
+from app.exporter import MetricExporter
 
 
 def get_configs():
-    parser = argparse.ArgumentParser(
-        description="AWS Cost Exporter, exposing AWS cost data as Prometheus metrics.")
-    parser.add_argument("-c", "--config", required=True,
-                        help="The config file (exporter_config.yaml) for the exporter")
+    parser = argparse.ArgumentParser(description="AWS Cost Exporter, exposing AWS cost data as Prometheus metrics.")
+    parser.add_argument(
+        "-c",
+        "--config",
+        required=True,
+        help="The config file (exporter_config.yaml) for the exporter",
+    )
     args = parser.parse_args()
 
-    if (not os.path.exists(args.config)):
-        logging.error(
-            "AWS Cost Exporter config file does not exist, or it is not a file!")
+    if not os.path.exists(args.config):
+        logging.error("AWS Cost Exporter config file does not exist, or it is not a file!")
         sys.exit(1)
 
     config = EnvYAML(args.config)
+    return config
 
-    # config validation
+
+def validate_configs(config):
     if config["group_by.enabled"]:
         if len(config["group_by.groups"]) < 1 or len(config["group_by.groups"]) > 2:
-            logging.error(
-                "If group_by is enabled, there should be at leaest one group, and at most two groups!")
+            logging.error("If group_by is enabled, there should be at least one group, and at most two groups!")
             sys.exit(1)
+        group_label_names = set()
+        for group in config["group_by.groups"]:
+            if group["label_name"] in group_label_names:
+                logging.error("Group label names should be unique!")
+                sys.exit(1)
+            else:
+                group_label_names.add(group["label_name"])
 
     if len(config["target_aws_accounts"]) == 0:
-        logging.error(
-            "There should be at leaest one target AWS accounts defined in the config!")
+        logging.error("There should be at least one target AWS accounts defined in the config!")
         sys.exit(1)
 
     labels = config["target_aws_accounts"][0].keys()
+    if group_label_names and (group_label_names & set(labels)):
+        logging.error("Some label names in group_by are the same as AWS account labels!")
+        sys.exit(1)
 
     if "Publisher" not in labels:
         logging.error("Publisher is a mandatory key in target_aws_accounts!")
@@ -45,11 +59,8 @@ def get_configs():
 
     for i in range(1, len(config["target_aws_accounts"])):
         if labels != config["target_aws_accounts"][i].keys():
-            logging.error(
-                "All the target AWS accounts should have the same set of keys (labels)!")
+            logging.error("All the target AWS accounts should have the same set of keys (labels)!")
             sys.exit(1)
-
-    return config
 
 
 def main(config):
@@ -60,7 +71,7 @@ def main(config):
         aws_access_secret=config["aws_access_secret"],
         aws_assumed_role_name=config["aws_assumed_role_name"],
         group_by=config["group_by"],
-        targets=config["target_aws_accounts"]
+        targets=config["target_aws_accounts"],
     )
     start_http_server(config["exporter_port"])
     app_metrics.run_metrics_loop()
@@ -70,4 +81,5 @@ if __name__ == "__main__":
     logger_format = "%(asctime)-15s %(levelname)-8s %(message)s"
     logging.basicConfig(level=logging.INFO, format=logger_format)
     config = get_configs()
+    validate_configs(config)
     main(config)
